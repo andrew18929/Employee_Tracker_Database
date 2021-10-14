95a04ea4d60473592267372331a773afaa04f807
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-10-14 11:07:56 -0500

    coded schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,36 @@
 -- drop database --
-
+DROP DATABASE IF EXISTS employee_tracker_db;
 -- create database --
+CREATE DATABASE employee_tracker_db;
 
 -- use database --
+USE employee_tracker_db;
 
 -- create department table --
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
 
 -- create role table --
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE SET NULL
+);
 
 -- create employee table --
+CREATE TABLE employee (
+    id INT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
+);
